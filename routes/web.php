43bfaf53fd5cda47bd95d38ed515d4da51941bf0
AnjuGurung <anjuugrg1215@gmail.com>
2025-02-23 23:40:48 +0545

    add orders

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\users\ProfileController;
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
@@ -16,15 +18,34 @@ Route::get('/register', [RegisterController::class, 'showRegistrationForm'])
 Route::post('/register', [RegisterController::class, 'register'])
     ->middleware('guest');
 
-Route::get('/', [ProductController::class, 'welcome'])-> name('home');
+Route::get('/', [ProductController::class, 'welcome'])->name('home');
 
+// Product view route for users
 Route::get('/product/{product}', [ProductController::class, 'show']);
 
+// Product view route for admin
 Route::get('/category/{id}', [CategoryController::class, 'show'])->name('category.show');
-Route::resource('categories', CategoryController::class);
+
+// Routes for order
+Route::post('/buy-now/{id}', [OrderController::class, 'buyNow'])->name('buy.now');
+Route::get('/order-success/{order}', [OrderController::class, 'orderSuccess'])->name('orders.success');
 
 // Protect routes that require authentication
 Route::middleware(['auth'])->group(function () {
-    Route::resource('products', ProductController::class);
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    
+    Route::resource('orders', OrderController::class);
+    Route::get('/profile', [ProfileController::class, 'profile'])->name('user.profile');
+
+    // admin access only
+    Route::middleware(['role:admin'])->group(function () {
+        Route::resource('categories', CategoryController::class);
+        Route::resource('products', ProductController::class);
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::get('/dashboard/profile', [ProfileController::class, 'profile'])->name('admin.profile');
+    });
+
+    // Routes accessible to regular users
+    Route::middleware(['role:user'])->group(function () {
+        //
+    });
 });
